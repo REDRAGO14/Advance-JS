576fe661da4303caad7697bb45746f901b68f11b
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-07 14:39:39 -0700

    worked on problem 2 of prototypal inheritance exploring passing argument to parent constractor function with child instance

--- AdvanceJS-Challenges/prototypalInheritance.js
+++ AdvanceJS-Challenges/prototypalInheritance.js
@@ -12,5 +12,30 @@ Dog.prototype.bark = function (){
     }
 
 let dog = new Dog()
-console.log(dog.makeSound())
+dog.makeSound()
+
+
+
+function Shape(color){
+    this.color = color
+}
+
+Shape.prototype.getColor = function(){
+    return `color: ${this.color}`
+}
+
+function Rectangle(width, height, color){
+    Shape.call(this, color)
+    this.width = width
+    this.height = height
+}
+
+Rectangle.prototype = Object.create(Shape.prototype)
+Rectangle.prototype.getArea = function(){
+    return `Area of Rectangle: ${this.width * this.height}`
+}
+
+let rec = new Rectangle(4,2, "blue")
+console.log(rec.getColor());
+console.log(rec.getArea());
 
